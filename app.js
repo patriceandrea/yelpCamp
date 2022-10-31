387f6dbff1ebdd2001b25d974ac3df08435246f7
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-31 16:19:54 -0400

    adds a POST request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -19,6 +20,8 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -26,6 +29,17 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds })
+})
+
+
+app.get('/campgrounds/new', async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -40,6 +54,7 @@ app.get('/makecampground', async (req, res) => {
   await camp.save()
   res.send(camp)
 })
+
 
 app.listen(3000, () => {
   console.log('Serving on Port 3000')
