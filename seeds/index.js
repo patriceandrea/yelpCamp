86253fc5c36233dedb2d57f3086b121516bd2de7
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-31 15:39:46 -0400

    seeds data

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -10,10 +9,12 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"))
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 })
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
   await Campground.deleteMany({});
@@ -21,7 +22,8 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`
     })
     await camp.save();
   }
@@ -29,4 +31,8 @@ const seedDB = async () => {
   c.save();
 }
 
-seedDB(); 
+seedDB()
+  .then(() => {
+    // mongoose.connection.close();
+    // mongoose.disconnect()
+  }); 
