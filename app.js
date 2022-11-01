8e358aee44664c258ab4ed2010b0525565cd8bdf
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-01 16:39:43 -0400

    adds morgan

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -22,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.render('home')
