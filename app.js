e1822e603a62ecefc691a32d2420f6ae1b106477
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-02 23:16:41 -0400

    adds basic error handler

--- app.js
+++ app.js
@@ -48,11 +48,16 @@ app.get('/campgrounds/new', async (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground)
-  await campground.save()
-  console.log(campground)
-  res.redirect(`/campgrounds/${campground._id}`)
+app.post("/campgrounds", async (req, res, next) => {
+  try {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    console.log(campground)
+    res.redirect(`/campgrounds/${campground._id}`)
+  }
+  catch (e) {
+    next(e)
+  }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -84,6 +89,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds')
 });
+
+app.use((err, req, res, next) => {
+  res.send('something went wrong!!')
+})
 
 app.listen(3000, () => {
   console.log('Serving on Port 3000')
