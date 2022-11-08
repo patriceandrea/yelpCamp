2a22e193a33a5b7690d52410d6677ff3b2c78fd7
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-08 14:17:44 -0500

    adds middleware to Delete functionality"

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampGroundSchema = new Schema({
   title: String,
@@ -14,6 +15,16 @@ const CampGroundSchema = new Schema({
     }
 
   ]
+})
+
+CampGroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
 })
 
 module.exports = mongoose.model('Campground', CampGroundSchema); 
