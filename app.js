404130cbe4fc989c3fbf00a3959a51da6106803c
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-11 15:08:57 -0500

    adds passport to server

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const cors = require('cors');
 const ExpressError = require('./helpers/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const LocalStrategy = require('passport-local')
+const passport = require('passport')
+const User = require('./models/user')
 
 const campgrounds = require('./routes/campground');
 const reviews = require('./routes/reviews')
@@ -59,6 +62,13 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
