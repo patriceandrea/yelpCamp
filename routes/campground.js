b13c42424e5f0a2cdbeee18d4ce2c830d765f0fb
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-14 14:51:59 -0500

    adds author to campground view

--- routes/campground.js
+++ routes/campground.js
@@ -30,6 +30,7 @@ const validateCampground = (req, res, next) => {
 
 router.post("/", validateCampground, isLoggedIn, catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body.campground)
+  campground.author = req.user._id;
   await campground.save()
   req.flash('success', 'Successfully made a new campground!');
   res.redirect(`/campgrounds/${campground._id}`)
@@ -37,7 +38,7 @@ router.post("/", validateCampground, isLoggedIn, catchAsync(async (req, res, nex
 
 router.get('/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findById(id).populate('reviews');
+  const campground = await Campground.findById(id).populate('reviews').populate('author');
   if (!campground) {
     req.flash('error', 'Cannot find that campground')
   }
