f91e8b9bd66351ef251a7602a5752ac4d54b0d73
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-19 11:59:19 -0500

    adds popup to the campground map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,9 @@
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
+
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  style: 'mapbox://styles/mapbox/outdoors-v10', // style URL
   center: JSON.parse(campground).geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
   projection: 'globe' // display the map as a 3D globe
@@ -11,5 +13,11 @@ map.on('style.load', () => {
   map.setFog({}); // Set the default atmosphere style
   new mapboxgl.Marker()
     .setLngLat(JSON.parse(campground).geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+          `<h3>${JSON.parse(campground).title}</h3><p>${JSON.parse(campground).location}</p>`
+        )
+    )
     .addTo(map);
 });
