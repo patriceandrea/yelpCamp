63693c48687e24223bb22059a13e9f6ae3ca8235
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-19 17:12:03 -0500

    adds basic clustering from data

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -15,10 +15,11 @@ map.on('load', () => {
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
   map.addSource('earthquakes', {
+
     type: 'geojson',
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+    data: parsedCampgrounds,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
